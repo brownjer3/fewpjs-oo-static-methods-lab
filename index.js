e4688ffbd490a338f592f702ac02a334bb7e93be
jerry <brownjer3@gmail.com>
2021-02-11 16:38:18 -0800

    commit tree b6dd39820f5ac1b24eae6d37cf4a724ffeb9b7fc

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1)
+  }
+
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-'/ ]+/g, '')
+  }
+  
+  static titleize(str) {
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    return str.split(" ").map((word) => {
+      if (word === str.split(" ")[0]) {
+        return Formatter.capitalize(word)
+      } else if (exceptions.includes(word)) {
+        return word
+      } else {
+        return Formatter.capitalize(word)
+      }
+    }).join(" ")
+  }
 }
